b0ac2dbbb7fa3671e079bfd864718f4a70518280
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-09-01 22:29:04 -0600

    Added Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
@@ -19,7 +20,12 @@ export default function RootLayout({
         'bg-white text-slate-900 antialiased light',
         inter.className
       )}>
-      <body className='min-h-screen pt-12 antialiased bg-slate-50'>{children}</body>
+      <body className='min-h-screen pt-12 antialiased bg-slate-50'>
+        <Navbar />
+        <div className='container h-full pt-12 mx-auto max-w-7xl'>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
