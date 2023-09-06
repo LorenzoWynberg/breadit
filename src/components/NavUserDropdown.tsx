d9103a7704761ac009900dacf78ac46d2f771bd1
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-09-05 18:51:11 -0600

    Logout

--- src/components/NavUserDropdown.tsx
+++ src/components/NavUserDropdown.tsx
@@ -2,9 +2,16 @@
 
 import { User } from 'next-auth';
 import { FC } from 'react';
-import { DropdownMenu } from './ui/dropdown-menu';
-import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
 import UserAvatar from './UserAvatar';
+import Link from 'next/link';
+import { signOut } from 'next-auth/react';
 
 interface NavUserDropdownProps {
   user: Pick<User, 'name' | 'email' | 'image'>;
@@ -19,6 +26,46 @@ const NavUserDropdown: FC<NavUserDropdownProps> = ({ user }) => {
           user={{ name: user.name || null, image: user.image || null }}
         />
       </DropdownMenuTrigger>
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="truncate text-sm text-zinc-700 w-[200px]">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/">Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/r/create">Create community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          onSelect={(event) => {
+            event.preventDefault();
+            signOut({
+              callbackUrl: `${window.location.origin}/sign-in`,
+            });
+          }}
+          className="cursor-pointer"
+        >
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
