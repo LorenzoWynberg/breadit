4941dd34b6ff73847fd5d44d99cc41d2ed612fb9
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-09-09 23:57:17 -0600

    Minor Fix

--- src/components/NavUserDropdown.tsx
+++ src/components/NavUserDropdown.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
-import UserAvatar from './UserAvatar';
+import { UserAvatar } from './UserAvatar';
 import Link from 'next/link';
 import { signOut } from 'next-auth/react';
 
