55e584811b0c1927cc1aa40b7233c6f4f2c76e99
LorenzoWynberg <lwcincr.dev@gmail.com>
2023-09-18 02:24:50 -0600

    Create Posts

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,8 +1,11 @@
 import SubscribeLeaveToggle from '@/components/SubscribeLeaveToggle';
+import ToFeedButton from '@/components/ToFeedButton';
+import { buttonVariants } from '@/components/ui/button';
 import { getAuthSession } from '@/lib/auth';
 import { db } from '@/lib/db';
 import { format } from 'date-fns';
 import type { Metadata } from 'next';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { ReactNode } from 'react';
 
@@ -60,7 +63,7 @@ const Layout = async ({
   return (
     <div className="h-full pt-12 mx-auto sm:container max-w-7xl">
       <div>
-        {/* TODO: <ToFeedButton /> */}
+        <ToFeedButton />
 
         <div className="grid grid-cols-1 py-6 md:grid-cols-3 gap-y-4 md:gap-x-4">
           <ul className="flex flex-col col-span-2 space-y-6">{children}</ul>
@@ -98,6 +101,15 @@ const Layout = async ({
                   subredditName={subreddit.name}
                 />
               ) : null}
+              <Link
+                className={buttonVariants({
+                  variant: 'outline',
+                  className: 'w-full mb-6',
+                })}
+                href={`r/${slug}/submit`}
+              >
+                Create Post
+              </Link>
             </dl>
           </div>
         </div>
